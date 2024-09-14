e8dae51449c842007df64cf26664fd8cf0e63f1d
HeckerCreatives <daniel.janbiengabrielle@gmail.com>
2024-09-14 12:02:59 +0800

    posh posh posh

---
+++ Assets/000 - KasubayCodeQuest/004 - Scripts/000 - GameManager/SchoolSceneData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SchoolSceneData", menuName = "BoukenQuest/Schoole/SceneData")]
+public class SchoolSceneData : ScriptableObject
+{
+    
+}
